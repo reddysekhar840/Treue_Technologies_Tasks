7f4833b190a030bbc0499ab686ac17cdf030fa73
reddysekhar840 <76563052+reddysekhar840@users.noreply.github.com>
2023-09-28 14:23:46 +0530

    Update OnlineParkingSystem.java

--- Task1/OnlineParkingSystem.java
+++ Task1/OnlineParkingSystem.java
@@ -8,39 +8,38 @@ public class OnlineParkingSystem
 	public static void start(ParkingSystem ps)
 	{
 		System.out.println("1. Display Available Parking Spots");
-        System.out.println("2. Book a Parking Spot");
-        System.out.println("3. Vacate a Parking Spot");
-        System.out.println("4. Exit\n");
-        
-        System.out.println("Select Your Choice : \n");
-        int choice = sc.nextInt();
-        switch(choice)
-        {
-        	case 1:
-        		ps.displayAvailableSlots();
-        		start(ps);
-        		break;
-        	case 2:
-        		System.out.println("Enter the spot number to book:\n");
-        		int bookSpot = sc.nextInt();
-        		ps.bookSlot(bookSpot,ps);
-        		start(ps);
-        		break;
-        	case 3: 
-        		System.out.println("Enter the spot number to vacate:\n");
-        		int vacateSpot = sc.nextInt();
-        		ps.vacateSlot(vacateSpot,ps);
-        		start(ps);
-        		break;
-        	case 4:
-        		System.out.println("Parking System Exited Successfully...\n");
-        		System.exit(0);
-        		break;
-        	default:
-        		System.out.println("Invalid Option \n Select below Options Only...\n");
-        		start(ps);
-        }
-    
+        	System.out.println("2. Book a Parking Spot");
+	        System.out.println("3. Vacate a Parking Spot");
+	        System.out.println("4. Exit\n");
+	        
+	        System.out.println("Select Your Choice :");
+	        int choice = sc.nextInt();
+	        switch(choice)
+	        {
+	        	case 1:
+	        		ps.displayAvailableSlots();
+	        		start(ps);
+	        		break;
+	        	case 2:
+	        		System.out.println("Enter the spot number to book:\n");
+	        		int bookSpot = sc.nextInt();
+	        		ps.bookSlot(bookSpot,ps);
+	        		start(ps);
+	        		break;
+	        	case 3: 
+	        		System.out.println("Enter the spot number to vacate:\n");
+	        		int vacateSpot = sc.nextInt();
+	        		ps.vacateSlot(vacateSpot,ps);
+	        		start(ps);
+	        		break;
+	        	case 4:
+	        		System.out.println("Parking System Exited Successfully...\n");
+	        		System.exit(0);
+	        		break;
+	        	default:
+	        		System.out.println("Invalid Option \n Select below Options Only...\n");
+	        		start(ps);
+		}
 	}
 	public static void main(String[] args)
 	{
